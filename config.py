50e665f1c6f192d65fc3020226e206994f85434c
Mangust <lakmoes@icloud.com>
2019-03-20 09:51:19 +0200

    Изменил базу

--- config.py
+++ config.py
@@ -5,10 +5,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_MAROON_URL', 'postgres://wesdftjbljfljz'
-                                                                             ':1d5b0aca62beac98834be760b12b'
-                                                                             'e0951b2e42e50d34d2e98dad969804a191b8'
-                                                                             '@ec2-79-125-6-250.eu-west-1.compute.amazonaws.com:5432/dsomromv0fr00')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_MAROON_URL', "postgres://uoodtxbeaoedvj"
+                                                                             ":3f2ec3f64379c27eeb5e5268c467"
+                                                                             "5ad546db45f241d5708d0608719de2435f93"
+                                                                             "@ec2-54-246-92-116.eu-west-1."
+                                                                             "compute.amazonaws.com:5432/d1hucge89ac9qc")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = basedir + '/images/'
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
